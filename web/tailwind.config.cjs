dd2c8b2acd8b97401cf33c54407946db9a9cd627
thalesluiz45 <thalesluiz45@hotmail.com>
2022-09-20 14:37:24 -0300

    First page completed

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,14 +5,16 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         galaxy:"url('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572fc 33%, #43e7ad 55%, #e1d55d 80.08%)'
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572fc 33%, #43e7ad 55%, #e1d55d 80.08%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       },
-      colors: {
-        
-      }
+      
     },
   },
   plugins: [],
